a5b7dbd9d8a10b3aab1550a97e37ec4f644217e7
sakamaki <sakamaki@globalwalkers.co.jp>
2022-10-14 16:51:58 +0900

    update:main

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import os
+
 from omegaconf import OmegaConf
 
 from recorder import Recorder
 
 if __name__ == "__main__":
+
     cfg = OmegaConf.load("config.yml")
+    os.makedirs(cfg.video.dest_dir, exist_ok=True)
     rec = Recorder(cfg)
 
     while True:
